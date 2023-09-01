951241eac19dd1e3467da1f150015def5a2bb0bd
cutep <cutep>
2023-09-01 15:44:25 +0800

    ...

--- webmain.py
+++ webmain.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB limit
 
 @app.route('/')
 def index():
@@ -9,11 +10,13 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['document']
+    filename = file.filename
+    size = file.content_length    
     # Process the uploaded file as needed
     # For example, save it to a specific location:
     file.save('file.pdf')
     # Return a response or redirect as necessary
-    return 'File uploaded successfully'
+    return 'file.pdf'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
